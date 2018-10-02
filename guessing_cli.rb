cb9a1abbe72e25e3116b2e3b23bb4b5df8bf7b4e
Austin Luft <ausluft@gmail.com>
2018-10-02 20:11:03 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,3 +1,15 @@
 # Code your solution here!
 
+def run_guessing_game
+  puts "Guess a number between 1 and 6."
+  user_input = ""
+  while user_input != "exit"
+    gets.chomp = user_input
+    puts user_input
+  end
+  puts "Goodbye!"
+end
+
+
+
 
